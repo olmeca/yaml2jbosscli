83ddee6c9d0e779d67aa34f060d80af26e578438
Rudi Angela <olmeca@me.com>
2017-08-16 07:56:42 +0200

    Fixed pom for unit tests

--- src/test/java/nl/olmeca/ContextTest.java
+++ src/test/java/nl/olmeca/ContextTest.java
@@ -1,5 +1,6 @@
 package nl.olmeca;
 
+
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -11,11 +12,21 @@ public class ContextTest {
 
     private Context context;
 
-    @Test public void test_context_finds_existing_key() {
+    @Test
+    public void test_context_finds_existing_key() {
         Map<String, String> map = new HashMap<>();
         map.put("een", "one");
         context = new Context(map);
         String resolved = context.resolve("zo <een> test");
         assertEquals("zo one test", resolved);
+    }
+
+    @Test public void test_context_finds_existing_keys() {
+        Map<String, String> map = new HashMap<>();
+        map.put("een", "one");
+        map.put("twee", "two");
+        context = new Context(map);
+        String resolved = context.resolve("zo <een> en <twee> test");
+        assertEquals("zo one en two test", resolved);
     }
 }
